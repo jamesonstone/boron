b66ad521cb09f8527a1c5947881419bd3090ad37
Jameson Stone <jamesonstone@Jamesons-Mac-mini.local>
2013-12-08 21:06:20 -0500

    fixed up tests so that they actually work

--- test_check_homepage.py
+++ test_check_homepage.py
@@ -1,13 +1,13 @@
 # Let's do this!
-# nosetests -v
+# nosetests -v --nocapture
 # http://selenium-python.readthedocs.org/en/latest/getting-started.html
-# 3
 # http://selenium.googlecode.com/git/docs/api/py/index.html
 
 import unittest
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+# from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 voxy = "http://master.pub.voxy.com/"
@@ -28,24 +28,19 @@ class CheckHomePage(unittest.TestCase):
         d.get(voxy)
         self.assertIn("VOXY", d.title)
 
-    #check 
+    #check "Start your free 7-day trial today!" button displays
     def test_homepage_elements(self):
     	d = self.driver
         d.get(voxy)
-        elem = d.find_element_by_xpath("//*[@id=\"fixed-login\"]/div/div/div/a")
-        self.check_element_exists(path)
+        d.find_element_by_xpath("//*[@id=\"fixed-login\"]/div/div/div/a")
+        print "found register button"
+        d.find_element_by_xpath("//*[@id=\"header_login_Btn\"]")
 
 
 
 
 
-    #method for checking element exists
-    def check_element_exists(x):
-        try:
-            webdriver.find_element_by_xpath(x)
-        except NoSuchElementException:
-            return False
-        return True
+
 
 
 
@@ -57,3 +52,22 @@ if __name__ == "__main__":
     unittest.main()
 
 
+
+
+'''
+    incorrect xpath
+    self.check_element_exists("//*[@id=\"fixed-login\"]/div/div/div/a_")
+    self.check_element_exists("//*[@id=\"fixed-login\"]/div/div/div/a")
+
+    def is_element_present(self, how, what):
+    try: self.driver.find_element(by=how, value=what)
+    except NoSuchElementException, e: return False
+    return True
+
+    def check_element_exists(self, x):
+    try:
+    if self.driver.find_element_by_xpath(x) is None:
+    print "Failed Cannot Find"
+    except NoSuchElementException:
+    return False
+'''
